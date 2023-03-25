1adeee34a56dea08f77c727f39ee656e9e3fd07c
ZinZero <realbloom0122@gmail.com>
2023-03-25 17:54:26 +0900

    4week done

--- hw04/hw04.ml
+++ hw04/hw04.ml
@@ -16,9 +16,50 @@ type expr =
 
 let rec eval (e : expr) = 
   match e with
-  | V v-> v
-  | E (op, n1, n2) -> 
-    | ADD ->
-      begin
-        match n1, n2 with
-        | Int i1, Int i2 ->
+  | V v -> v
+  | E (op, n1, n2) ->
+    begin
+        match op with 
+        | ADD ->
+            begin 
+                match (eval n1), (eval n2) with
+                | Int i1, Int i2 -> Int (i1 + i2)
+                | Float f1, Float f2 -> Float (f1 +. f2)
+                | Int i, Float f -> Float ((float_of_int i) +. f)
+                | Float f, Int i -> Float (f +. (float_of_int i))
+                | _, Err -> Err
+                | Err, _  -> Err
+            end    
+        | SUB ->
+            begin
+                match (eval n1), (eval n2) with
+                | Int i1, Int i2 -> Int (i1 - i2)
+                | Float f1, Float f2 -> Float (f1 -. f2)
+                | Int i, Float f
+                | Float f, Int i -> Float ((float_of_int i) -. f)
+                | _, Err -> Err
+                | Err, _ -> Err
+            end
+        | MUL ->
+            begin
+                match (eval n1), (eval n2) with
+                | Int i1, Int i2 -> Int (i1 * i2)
+                | Float f1, Float f2 -> Float (f1 *. f2)
+                | Int i, Float f
+                | Float f, Int i -> Float ((float_of_int i) *. f)
+                | _, Err -> Err
+                | Err, _ -> Err
+            end
+        | DIV ->
+            begin
+                match (eval n1), (eval n2) with
+                | _, Int 0 -> Err
+                | _, Float 0.0 -> Err
+                | Int i1, Int i2 -> Int (i1 / i2)
+                | Float f1, Float f2 -> Float (f1 /. f2)
+                | Int i, Float f
+                | Float f, Int i -> Float ((float_of_int i) /. f)
+                | _, Err -> Err
+                | Err, _ -> Err
+            end
+    end
